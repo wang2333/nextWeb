a4ce7ae02d94b58c7f76d37df5d2346d6762359f
wangliang <wangliang@houseitech.com>
2023-10-31 10:04:22 +0800

    1

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const path = require("path")
 
 module.exports = {
   reactStrictMode: true,
-  output: "out",
+  output: "export",
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
