2f40ecf1f71a1cc9a71e9a7c590a02818fc40e6b
wangliang <wangliang@houseitech.com>
2023-11-08 09:57:03 +0800

    Add meta viewport tag and update font size in globals.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/styles/bootstrap.min.css"
 import "@/styles/globals.css"
 import { Metadata } from "next"
+import Head from "next/head"
 
 import { siteConfig } from "@/config/site"
 import Footer from "@/components/layout/Footer"
@@ -27,6 +28,9 @@ interface RootLayoutProps {
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <body className='antialiased" bg-background min-h-screen font-sans'>
         <div className="relative flex min-h-screen flex-col">
           <Header />
