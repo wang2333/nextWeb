1fd135645b681a691dfe3ab7ae9df2be130897ee
wangliang <wangliang@houseitech.com>
2023-11-14 18:05:35 +0800

    Add links to footer items and import next/navigation

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,5 +1,8 @@
+"use client"
+
 import React from "react"
 import Image from "next/image"
+import { useRouter } from "next/navigation"
 import { Col, Container, Row } from "react-bootstrap"
 
 import { Icons } from "@/components/icons"
@@ -8,33 +11,82 @@ const footData = [
   {
     title: "产品开发",
     childRen: [
-      "视觉呈现",
-      "网站开发与重构",
-      "应用软件",
-      "微信小程序",
-      "移动应用",
-      "服务器与硬件",
+      {
+        title: "视觉呈现",
+        herf: "/product/visual",
+      },
+      {
+        title: "网站开发与重构",
+        herf: "/product/web",
+      },
+      {
+        title: "应用软件",
+        herf: "/product/application",
+      },
+      {
+        title: "微信小程序",
+        herf: "/product/weChat",
+      },
+      {
+        title: "移动应用",
+        herf: "/product/mobile",
+      },
+      {
+        title: "服务器与硬件",
+        herf: "/product/server",
+      },
     ],
   },
   {
     title: "解决方案",
     childRen: [
-      "数据储存与计算",
-      "服务协议",
-      "教育/企业/商业网络构建",
-      "大数据分析与处理",
+      {
+        title: "广告传媒数据存储与计算",
+        herf: "/plan/dataStorage",
+      },
+      {
+        title: "机房建设",
+        herf: "/plan/protocol",
+      },
+      {
+        title: "教育/企业/商业网络构建",
+        herf: "/plan/network",
+      },
+      {
+        title: "大数据分析与处理",
+        herf: "/plan/bigData",
+      },
     ],
   },
   {
     title: "智云动态",
-    childRen: ["2023互联网大会", "大数据与AI智能", "参加计算机算法活动"],
+    childRen: [
+      {
+        title: "2023互联网大会",
+        herf: "/news/detail",
+      },
+      {
+        title: "大数据与AI智能",
+        herf: "/news/detail",
+      },
+      {
+        title: "参加计算机算法活动",
+        herf: "/news/detail",
+      },
+    ],
   },
   {
     title: "联系洽谈",
-    childRen: ["+15926269363"],
+    childRen: [
+      {
+        title: "+15926269363",
+      },
+    ],
   },
 ]
 function Footer() {
+  const router = useRouter()
+
   return (
     <div className=" bg-black1 ">
       <Container className="container pt-16 ">
@@ -68,8 +120,14 @@ function Footer() {
                     <div className="childRen">
                       {item.childRen.map((child) => {
                         return (
-                          <div className="child mb-2" key={child}>
-                            {child}
+                          <div
+                            className="child mb-2 cursor-pointer"
+                            key={child.title}
+                            onClick={() =>
+                              child.herf && router.push(child.herf)
+                            }
+                          >
+                            {child.title}
                           </div>
                         )
                       })}
